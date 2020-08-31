22376f61e1eea54c374b637a9f8208919513d6b0
vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-08-30 22:03:00 -0300

    list transactions and block outcome value

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-const transactions = [];
-
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    return response.json({ message: 'hello' });
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -22,14 +25,15 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    const transaction = {
-      id: uuid(),
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
       title,
       value,
       type,
-    };
-
-    transactions.push(transaction);
+    });
 
     return response.json(transaction);
   } catch (err) {
